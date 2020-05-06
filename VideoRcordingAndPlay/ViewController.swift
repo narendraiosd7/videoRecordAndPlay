b95039ac57ab91dc681d374befcfb6d437ac107e
varalakshmi-iOS <varalakshmi1318@gmail.com>
2020-05-06 16:16:41 +0530

    Initial Commit

---
+++ VideoRcordingAndPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoRcordingAndPlay
+//
+//  Created by Vadde Narendra on 5/6/20.
+//  Copyright © 2020 Narendra Vadde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
